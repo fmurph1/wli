d9c1243565c36d490c09a2eb48bfc9917a88b064
fmurph1 <32552121+fmurph1@users.noreply.github.com>
2024-05-19 21:45:58 +0200

    Update installlinux.bat

--- installlinux.bat
+++ installlinux.bat
@@ -2,6 +2,11 @@ bcdedit -set TESTSIGNING OFF
 start %~dp0grub2win\G2WInstall.exe
 echo Press any key after grub2win installation is complete.
 pause
-xcopy /s /i "%~dp0grub2" "C:\grub2" /Y
-xcopy /s /i "%~dp0linuxmint-21.2-cinnamon-64bit.iso" "C:\" /Y
+xcopy /i "%~dp0grub.cfg" "C:\grub2\grub.cfg" /Y
+md C:\grub2\windata\customconfigs
+"">C:\grub2\windata\customconfigs\BootAnISOfile.cfg
+xcopy /i "%~dp0BootAnISOfile.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
+
+powershell -Command "Start-BitsTransfer -Source "https://mirrors.layeronline.com/linuxmint/stable/21.3/linuxmint-21.3-cinnamon-64bit.iso" -Destination "C:\linuxmint-21.3-cinnamon-64bit.iso""
+
 shutdown /r
