404d22a09eceba42ff004de3c639df8ce765c4cf
fmurph1 <32552121+fmurph1@users.noreply.github.com>
2024-05-21 07:27:28 +0200

    Update installlinux.bat

--- installlinux.bat
+++ installlinux.bat
@@ -5,8 +5,34 @@ pause
 xcopy /i "%~dp0grub.cfg" "C:\grub2\grub.cfg" /Y
 md C:\grub2\windata\customconfigs
 "">C:\grub2\windata\customconfigs\BootAnISOfile.cfg
-xcopy /i "%~dp0BootAnISOfile.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
 
+ECHO OFF
+
+ECHO ...............................................
+ECHO Select your OS
+ECHO ...............................................
+ECHO 1 - Linux Mint 21.3 Cinnamon
+ECHO 2 - Linux Mint 21.3 XFCE
+ECHO 3 - Ubuntu 24.04 LTS
+
+SET /P M=Type a number then press ENTER:
+IF %M%==1 GOTO CIN
+IF %M%==2 GOTO XFCE
+IF %M%==3 GOTO UBU
+
+:CIN
+xcopy /i "%~dp0BootAnISOfile-cin.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
 powershell -Command "Start-BitsTransfer -Source "https://mirrors.layeronline.com/linuxmint/stable/21.3/linuxmint-21.3-cinnamon-64bit.iso" -Destination "C:\linuxmint-21.3-cinnamon-64bit.iso""
+GOTO SHUTDOWN
 
+:XFCE
+xcopy /i "%~dp0BootAnISOfile-xfce.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
+powershell -Command "Start-BitsTransfer -Source "https://mirrors.layeronline.com/linuxmint/stable/21.3/linuxmint-21.3-xfce-64bit.iso" -Destination "C:\linuxmint-21.3-xfce-64bit.iso""
+GOTO SHUTDOWN
+
+:UBU
+xcopy /i "%~dp0BootAnISOfile-ubu.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
+powershell -Command "Start-BitsTransfer -Source "https://mirrors.edge.kernel.org/ubuntu-releases/24.04/ubuntu-24.04-desktop-amd64.iso" -Destination "C:\ubuntu-24.04-desktop-amd64.iso""
+
+:SHUTDOWN
 shutdown /r
