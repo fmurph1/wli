d85be8bec4ebad425e4634e05037cbf49a110212
fmurph1 <32552121+fmurph1@users.noreply.github.com>
2024-10-04 21:46:46 +0200

    Add files via upload

--- installlinux.bat
+++ installlinux.bat
@@ -11,9 +11,9 @@ ECHO OFF
 ECHO ...............................................
 ECHO Select your OS
 ECHO ...............................................
-ECHO 1 - Linux Mint 21.3 Cinnamon
-ECHO 2 - Linux Mint 21.3 XFCE
-ECHO 3 - Ubuntu 24.04 LTS
+ECHO 1 - Linux Mint 22 Cinnamon
+ECHO 2 - Linux Mint 22 XFCE
+ECHO 3 - Ubuntu 24.04.1 LTS
 
 SET /P M=Type a number then press ENTER:
 IF %M%==1 GOTO CIN
@@ -22,17 +22,17 @@ IF %M%==3 GOTO UBU
 
 :CIN
 xcopy /i "%~dp0BootAnISOfile-cin.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
-powershell -Command "Start-BitsTransfer -Source "https://mirrors.layeronline.com/linuxmint/stable/21.3/linuxmint-21.3-cinnamon-64bit.iso" -Destination "C:\linuxmint-21.3-cinnamon-64bit.iso""
+powershell -Command "Start-BitsTransfer -Source "https://mirrors.layeronline.com/linuxmint/stable/22/linuxmint-22-cinnamon-64bit.iso" -Destination "C:\linuxmint-22-cinnamon-64bit.iso""
 GOTO SHUTDOWN
 
 :XFCE
 xcopy /i "%~dp0BootAnISOfile-xfce.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
-powershell -Command "Start-BitsTransfer -Source "https://mirrors.layeronline.com/linuxmint/stable/21.3/linuxmint-21.3-xfce-64bit.iso" -Destination "C:\linuxmint-21.3-xfce-64bit.iso""
+powershell -Command "Start-BitsTransfer -Source "https://mirrors.layeronline.com/linuxmint/stable/22/linuxmint-22-xfce-64bit.iso" -Destination "C:\linuxmint-22-xfce-64bit.iso""
 GOTO SHUTDOWN
 
 :UBU
 xcopy /i "%~dp0BootAnISOfile-ubu.cfg" "C:\grub2\windata\customconfigs\BootAnISOfile.cfg" /Y
-powershell -Command "Start-BitsTransfer -Source "https://mirrors.edge.kernel.org/ubuntu-releases/24.04/ubuntu-24.04-desktop-amd64.iso" -Destination "C:\ubuntu-24.04-desktop-amd64.iso""
+powershell -Command "Start-BitsTransfer -Source "https://mirrors.edge.kernel.org/ubuntu-releases/24.04.1/ubuntu-24.04.1-desktop-amd64.iso" -Destination "C:\ubuntu-24.04.1-desktop-amd64.iso""
 
 :SHUTDOWN
 shutdown /r
